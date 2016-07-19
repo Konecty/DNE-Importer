c8789d8285221ce34b02e3cbb21942e9e3f85f53
Daniel Fleck <danielfleck@live.ca>
2016-07-19 13:09:55 -0300

    cria metodos para popular localidade e bairro

--- open.js
+++ open.js
@@ -2,34 +2,127 @@ var fs = require('fs');
 var stream = require('stream');
 var es = require('event-stream');
 var mongoose = require('mongoose');
-var highland = require('highland');
+var iconv = require('iconv-lite');
 
 mongoose.connect('mongodb://localhost:27017/importer');
 
 var Logradouro = require('./models/logradouro_model.js')
+var Localidade = require('./models/localidade_model.js')
+var Bairro = require('./models/bairro_model.js')
+
 // Ler arquivos da pasta
+var pattern_logradouro = new RegExp("(?!LOG_)LOGRADOURO")
+var pattern_localidade = new RegExp("(?!LOG_)LOCALIDADE")
+var pattern_bairro = new RegExp("(?!LOG_)BAIRRO")
 fs.readdir('./resources', function(err, files){
-  var pattern_logradouro = new RegExp("(?!LOG_)LOGRADOURO")
   files.forEach(function(file){
+    console.log(file);
     if(pattern_logradouro.test(file)){
       OpenLogradouro(file);
     }
+    if(pattern_localidade.test(file)){
+      OpenLocalidade(file);
+    }
+    if(pattern_bairro.test(file)){
+      OpenBairro(file);
+    }
   })
+  console.log('Importação concluida com sucesso!');
 });
 
-function OpenLogradouro(file){
-
+function OpenBairro(file){
+  var count = 0;
   console.log(file)
-  var s = fs.createReadStream('./resources/'+file)
+  var s = fs.createReadStream('./resources/'+file, {autoClose: true})
+    .pipe(iconv.decodeStream('ISO-8859-1'))
     .pipe(es.split())
     .pipe(es.mapSync(function(line){
-        console.log(line)
-        // pause o readstream
+        // pause the readstream
+        s.pause();
+        var attributes = line.split('@');
+        var bairro = new Bairro();
+        if(attributes.length == 5){
+          count++;
+          bairro.id = attributes[0];
+          bairro.state = attributes[1];
+          bairro.city = attributes[2];
+          bairro.district = attributes[3];
+          bairro.districtAbbr = attributes[4];
+          bairro.save(function(err, bro){
+            s.resume();
+          });
+        }
+    })
+    .on('error', function (error) {
+        console.log("Caught", error);
+        s.destroy();
+        // s.close();
+        console.log('Read entire file with error. Lines Read: ', count);
+    })
+    .on('end', function(){
+      //s.close();
+      s.destroy();
+      console.log('Read entire file. Lines Read: ', count);
+      return true;
+    })
+  );
+}
+
+function OpenLocalidade(file){
+  var count = 0;
+  console.log(file)
+  var s = fs.createReadStream('./resources/'+file, {autoClose: true})
+    .pipe(iconv.decodeStream('ISO-8859-1'))
+    .pipe(es.split())
+    .pipe(es.mapSync(function(line){
+        // pause the readstream
+        s.pause();
+        var attributes = line.split('@');
+        var localidade = new Localidade();
+        if(attributes.length == 9){
+          count++;
+          localidade.id = attributes[0];
+          localidade.state = attributes[1];
+          localidade.city = attributes[2];
+          localidade.postalCode = attributes[3];
+          localidade.status = attributes[4];
+          localidade.type = attributes[5];
+          localidade.subordination = attributes[6];
+          localidade.cityAbbr = attributes[7];
+          localidade.cityIbgeCode = attributes[8];
+          localidade.save(function(err, lgr){
+            s.resume();
+          });
+        }
+    })
+    .on('error', function (error) {
+        console.log("Caught", error);
+        s.destroy();
+        // s.close();
+        console.log('Read entire file with error. Lines Read: ', count);
+    })
+    .on('end', function(){
+      //s.close();
+      s.destroy();
+      console.log('Read entire file. Lines Read: ', count);
+      return true;
+    })
+  );
+}
+
+function OpenLogradouro(file){
+  var count = 0;
+  console.log(file)
+  var s = fs.createReadStream('./resources/'+file, {autoClose: true})
+    .pipe(iconv.decodeStream('ISO-8859-1'))
+    .pipe(es.split())
+    .pipe(es.mapSync(function(line){
+        // pause the readstream
         s.pause();
         var attributes = line.split('@');
         var logradouro = new Logradouro();
-        
         if(attributes.length == 11){
+          count++;
           logradouro.state = attributes[1];
           logradouro.city = attributes[2];
           logradouro.startNeighbourhood = attributes[3];
@@ -41,13 +134,20 @@ function OpenLogradouro(file){
           logradouro.status = attributes[9];
           logradouro.placeAbbr = attributes[10];
           logradouro.save(function(err, lgr){
-            // resume readstream
             s.resume();
           });
         }
     })
+    .on('error', function (error) {
+        console.log("Caught", error);
+        s.close();
+        console.log('Read entire file with error. Lines Read: ', count);
+    })
     .on('end', function(){
-        console.log('Read entire file.');
+      s.close();
+      s.destroy();
+      console.log('Read entire file. Lines Read: ', count);
+      return true;
     })
   );
 }
